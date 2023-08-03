664a80f319775c6425c8ed4acdabe8aa0bf894f8
Alexey Pyzhianov <alex.pyzhianov@gmail.com>
2023-08-03 16:56:09 +0300

    feat: Initial setups
    - Custom copilot
    - No snippets
    - No sessions
    - Colorscheme configuration
    - Multiple languages support

---
+++ lua/plugins/disable.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "echasnovski/mini.pairs",
+    enabled = false,
+  },
+
+  {
+    "L3MON4D3/LuaSnip",
+    enabled = false,
+  },
+  {
+    "rafamadriz/friendly-snippets",
+    enabled = false,
+  },
+  {
+    "folke/persistence.nvim",
+    enabled = false,
+  },
+}
